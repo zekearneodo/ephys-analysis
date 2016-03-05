15a22f0cc33c62b8c1214b45584431305c8695e2
Justin Kiggins <justin.kiggins@gmail.com>
2016-03-05 11:47:10 -0800

    added setup.py to allow installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ephys-analysis',
+    version='0.0.1',
+    description='ephys analysis for phy data ',
+    author='Justin Kiggins',
+    author_email='justin.kiggins@gmail.com',
+    packages=['ephys',
+              ],
+)
