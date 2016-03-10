604004ba265c2ad657ac3839b5ac406d875346b9
Marvin T <nivram@berkeley.edu>
2016-03-10 09:52:05 -0800

    more working

--- ephys/rigid_pandas.py
+++ ephys/rigid_pandas.py
@@ -2,16 +2,81 @@ import h5py
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+import core
+
+def kwik2rigid_pandas(block_path):
+    spikes = core.get_spikes(block_path)
+    stims = get_acute_stims(block_path)
+    spikes['stim_name'], spikes['stim_presentation'], spikes['stim_time_stamp'] = zip(*spikes["time_samples"].map(_EventAligner(stims).stim_checker))
+    spikes['stim_aligned_time_stamp'] = spikes['time_samples'].values.astype('int') - spikes['stim_time_stamp'].values
+    del spikes['time_samples']
+    del spikes['stim_time_stamp']
+    timestamp2time(spikes, core.get_fs(block_path), 'stim_aligned_time_stamp', 'stim_aligned_time')
+    return spikes, stims
+
+def get_acute_stims(block_path):
+    '''
+    Fast code to load up stimuli information for an acute recording
+
+    Parameters
+    -------
+    block_path : str
+        the path to the block
+
+    Returns
+    ------
+
+    trials : pandas dataframe
+
+    Columns
+    ------
+    '''
+    with h5py.File(core.get_kwik(block_path), 'r') as f:
+        with f['event_types']['DigMark']['codes'].astype('str'):
+            stims = pd.DataFrame({ 'time_samples' : f['event_types']['DigMark']['time_samples'],
+                                   'code' : f['event_types']['DigMark']['codes'][:] })
+        #assumes one start and one end for each trial
+        stims.loc[stims.code == '<', 'stim_end_time_stamp'] = stims[stims['code'] == '>']['time_samples'].values 
+        stims = stims[stims['code'] == '<']
+        stims['stim_name'] = f['event_types']['Stimulus']['text'][1::2]
+    stims['stim_presentation'] = stims[stims['code'] == '<']['stim_name'].map(_EventCounter().count)
+    stims.reset_index(drop=True, inplace=True)
+    del stims['code']
+    return stims
+
+def timestamp2time(df, sample_rate, time_stamp_label, time_label):
+    df[time_stamp_label] = df[time_stamp_label].values / sample_rate
+    df.rename(columns={time_stamp_label: time_label}, inplace=True)
+
+
+def raster_by_unit(spikes, cluster, sample_rate, stim_length, window_size=1):
+    sns.set_context("notebook", font_scale=1.5, rc={'lines.markeredgewidth': .1, 'patch.linewidth':1})
+    sns.set_style("white")
+    num_repeats = np.max(spikes['stim_presentation'].values)
+    num_stims = len(np.unique(spikes["stim_name"]))
+    g = sns.FacetGrid(spikes[spikes['cluster']==cluster], 
+        col="stim_name", col_wrap=int(np.sqrt(num_stims)), 
+        xlim=(-window_size, stim_length+window_size), ylim=(0,num_repeats));
+    g.map(plt.scatter, "stim_aligned_time", "stim_presentation", marker='|')
+    for ax in g.axes.flat:
+        ax.plot((0, 0), (0, num_repeats), c=".2", alpha=.5)
+        ax.plot((stim_length, stim_length), (0, num_repeats), c=".2", alpha=.5)
+    g = g.set_titles("cluster %d, stim: {col_name}" % (cluster))
+    plt.savefig('test.png')
+
 
 from collections import Counter
-class StimCounter(Counter):
+class _EventCounter(Counter):
     def count(self, key):
         self[key] += 1
         return self[key] - 1
 
-class Stims(object):
-    # ['code', 'time_stamp', 'stim_end_time_stamp', 'stim_name', 'stim_presentation']
+class _EventAligner(object):
+    # ['time_stamp', 'stim_end_time_stamp', 'stim_name', 'stim_presentation']
+    # TODO: make robust to multiple recordings
     # TODO: duplicate spikes that are <2 sec from 2 stimuli
+    # TODO: generalize for any event type
     def __init__(self, stims, stim_index=0):
         self.stim_index = stim_index
         self.stims = stims
@@ -19,13 +84,13 @@ class Stims(object):
         self.cur_stim = self.stims.loc[self.stim_index].values
         self.next_stim = self.stims.loc[self.stim_index+1].values
     def stim_checker(self, time_stamp):
-        if time_stamp < self.cur_stim[1]:
-            if self.stim_index == 0 or self.cur_stim[1] - time_stamp < time_stamp - self.prev_stim[2]:
-                return self.cur_stim[[3, 4, 1]]
+        if time_stamp < self.cur_stim[0]:
+            if self.stim_index == 0 or self.cur_stim[0] - time_stamp < time_stamp - self.prev_stim[1]:
+                return self.cur_stim[[2, 3, 0]]
             else:
-                return self.prev_stim[[3, 4, 1]]
-        elif time_stamp < self.cur_stim[2]:
-            return self.cur_stim[[3, 4, 1]]
+                return self.prev_stim[[2, 3, 0]]
+        elif time_stamp < self.cur_stim[1]:
+            return self.cur_stim[[2, 3, 0]]
         else:
             if self.stim_index + 1 < len(self.stims):
                 #print time_stamp, self.stim_index
@@ -38,40 +103,4 @@ class Stims(object):
                     self.next_stim = None
                 return self.stim_checker(time_stamp)
             else:
-                return self.cur_stim[[3, 4, 1]]
-
-def load_kwik(kwikfile):
-    with h5py.File(kwikfile, 'r') as f:
-        sample_rate = None
-        for recording in f['recordings']:
-            assert sample_rate is None or sample_rate == f['recordings'][recording].attrs['sample_rate']
-            sample_rate = f['recordings'][recording].attrs['sample_rate']
-        spikes = pd.DataFrame({ 'time_stamp' : f['channel_groups']['0']['spikes']['time_samples'],
-                                'cluster' : f['channel_groups']['0']['spikes']['clusters']['main'] })
-        cluster_groups = {}
-        for cluster in f['channel_groups']['0']['clusters']['main'].keys():
-            cluster_groups[int(cluster)] = f['channel_groups']['0']['clusters']['main'][cluster].attrs['cluster_group']
-        spikes['cluster_group'] = spikes['cluster'].map(cluster_groups)
-        
-        with f['event_types']['DigMark']['codes'].astype('str'):
-            stims = pd.DataFrame({ 'time_stamp' : f['event_types']['DigMark']['time_samples'],
-                                   'code' : f['event_types']['DigMark']['codes'][:] })
-        stims.loc[stims.code == '<', 'stim_end_time_stamp'] = stims[stims['code'] == '>']['time_stamp'].values
-        stims = stims[stims['code'] == '<']
-        stims['stim_name'] = f['event_types']['Stimulus']['text'][1::2]
-    stims['stim_presentation'] = stims[stims['code'] == '<']['stim_name'].map(StimCounter().count)
-    stims.reset_index(drop=True, inplace=True)
-    spikes['stim_name'], spikes['stim_presentation'], spikes['stim_time_stamp'] = zip(*spikes["time_stamp"].map(Stims(stims).stim_checker))
-    spikes['stim_aligned_time_stamp'] = spikes['time_stamp'].values.astype('int') - spikes['stim_time_stamp'].values
-    spikes['stim_aligned_time'] = spikes['stim_aligned_time_stamp'].values / sample_rate
-    return spikes, stims, sample_rate
-
-def raster_by_unit(spikes, cluster, sample_rate):
-    sns.set_context("notebook", font_scale=1.5, rc={'lines.markeredgewidth': .1, 'patch.linewidth':1})
-    sns.set_style("white")
-    g = sns.FacetGrid(spikes[spikes['cluster']==cluster], col="stim_name", col_wrap=3, xlim=(-.5, 1), ylim=(0,num_repeats));
-    g.map(plt.scatter, "stim_aligned_time", "stim_presentation", marker='|')
-    for ax in g.axes.flat:
-        ax.plot((0, 0), (0, num_repeats), c=".2", alpha=.5)
-        ax.plot((stim_length, stim_length), (0, num_repeats), c=".2", alpha=.5)
-    g = g.set_titles("cluster %d, stim: {col_name}" % (cluster))
+                return self.cur_stim[[2, 3, 0]]
--- ephys/rigid_pandas_test.py
+++ ephys/rigid_pandas_test.py
@@ -1,17 +1,32 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import rigid_pandas
+import core
 from timeit import default_timer as timer
 
-kwikfile = '/home/mthielk/Data/B979/acute/analysis/klusta/Pen04_Lft_AP2555_ML500__Site02_Z2688__st979_cat_P04_S02_1/st979_cat_P04_S02_1.kwik'
-kristas_kwikfile = '/mnt/cube/ephys-example-data/Pen01_Lft_AP100_ML1300__Site03_Z2500__B1056_cat_P01_S03_1/B1056_cat_P01_S03_1.kwik'
+kwikfile = '/home/mthielk/Data/B979/acute/analysis/klusta/Pen04_Lft_AP2555_ML500__Site02_Z2688__st979_cat_P04_S02_1'
+kristas_kwikfile = '/mnt/cube/ephys-example-data/Pen01_Lft_AP100_ML1300__Site03_Z2500__B1056_cat_P01_S03_1'
+
+block_path = kristas_kwikfile
 
 start = timer()
-spikes, stims, sample_rate = rigid_pandas.load_kwik(kristas_kwikfile)
+spikes, stims = rigid_pandas.kwik2rigid_pandas(block_path)
 end = timer()
 print end-start
-#print spikes
+print spikes.head()
+print stims.head()
 
-spikes['morph_dim'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[0:2]
-spikes['morph_pos'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[2:].astype(int)
-good_spikes = spikes[spikes['cluster_group'] == 2]
+#spikes['morph_dim'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[0:2]
+#spikes['morph_pos'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[2:].astype(int)
+
+
 #rec_stims = good_spikes[good_spikes.stim_name.str.contains('rec')]
 
+clusters = core.get_clusters(block_path)
+fs = core.get_fs(block_path)
+stim_length = (stims.stim_end_time_stamp.head(1).values[0] - stims.time_samples.head(1).values[0]) / fs
+print clusters.head()
+cluster = clusters[clusters['quality']=="Good"]['cluster'].iloc[0]
+print stim_length
+rigid_pandas.raster_by_unit(spikes, cluster, fs, stim_length)
