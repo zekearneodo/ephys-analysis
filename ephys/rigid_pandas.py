6646e2a55ba64a826b4ca0ed29e505d806d13b3a
Marvin T <nivram@berkeley.edu>
2016-03-11 18:12:41 -0800

    documentation and testing

--- ephys/rigid_pandas.py
+++ ephys/rigid_pandas.py
@@ -6,16 +6,34 @@ import matplotlib.pyplot as plt
 import core
 
 def kwik2rigid_pandas(block_path):
+    '''
+    loads data in a manner that is very useful for acute experiments
+
+    Parameters
+    ------
+    block_path : str
+        the path to the block
+
+    Returns
+    ------
+    spikes : Pandas.DataFrame
+        columns: cluster, recording, stim_name, stim_presentation, 
+                 stim_duration, stim_aligned_time
+    stims : Pandas.DataFrame
+        columns: stim_start, stim_end, stim_name, stim_presentation, 
+                 stim_duration
+
+    '''
     spikes = core.load_spikes(block_path)
     stims = load_acute_stims(block_path)
-    spikes = spikes.join(align_events(spikes, stims))
-    spikes['stim_aligned_time'] = spikes['time_samples'].values.astype('int') - spikes['stim_start'].values
-    spikes['stim_duration'] = spikes['stim_end'] - spikes['stim_start']
     fs = core.get_fs(block_path)
-    timestamp2time(spikes, fs, 'stim_duration')
+    stims['stim_duration'] = stims['stim_end'] - stims['stim_start']
+    timestamp2time(stims, fs, 'stim_duration')
+    spikes = spikes.join(align_events(spikes, stims))
+    spikes['stim_aligned_time'] = (spikes['time_samples'].values.astype('int') -
+                                   spikes['stim_start'].values)
     del spikes['time_samples']
     del spikes['stim_start']
-    del spikes['stim_end']
     timestamp2time(spikes, fs, 'stim_aligned_time')
     return spikes, stims
 
@@ -47,18 +65,39 @@ def load_acute_stims(block_path):
     '''
     with h5py.File(core.find_kwik(block_path), 'r') as f:
         with f['event_types']['DigMark']['codes'].astype('str'):
-            stims = pd.DataFrame({ 'stim_start' : f['event_types']['DigMark']['time_samples'],
-                                   'code' : f['event_types']['DigMark']['codes'][:] })
+            stims = pd.DataFrame({
+                'stim_start' : f['event_types']['DigMark']['time_samples'],
+                'code' : f['event_types']['DigMark']['codes'][:] })
         #assumes one start and one end for each trial
-        stims.loc[stims.code == '<', 'stim_end'] = stims[stims['code'] == '>']['stim_start'].values 
+        stims.loc[stims.code == '<', 'stim_end'] = \
+            stims[stims['code'] == '>']['stim_start'].values
         stims = stims[stims['code'] == '<']
         stims['stim_name'] = f['event_types']['Stimulus']['text'][1::2]
-    stims['stim_presentation'] = stims[stims['code'] == '<']['stim_name'].map(_EventCounter().count)
+    stims['stim_presentation'] = stims['stim_name'].map(_EventCounter().count)
     stims.reset_index(drop=True, inplace=True)
     del stims['code']
     return stims
 
-def timestamp2time(df, sample_rate, time_stamp_label, time_label=None, inplace=True):
+def timestamp2time(df, sample_rate, time_stamp_label, 
+                   time_label=None, inplace=True):
+    '''
+    Converts a column from time samples to time in seconds
+
+    Parameters
+    ------
+    df : Pandas.DataFrame
+        DataFrame containing column indicated by time_stamp_label
+        df will be modified and not returned
+    sample_rate : int
+        sample rate, from core.load_fs()
+    time_stamp_label : str
+        label of column with time stamp DataFrame
+    time_label : str
+        label of target column
+        if None (default) leaves time data in df[time_stamp_label]
+    inplace : boolean
+        whether to overwrite df[time_stamp_label]
+    '''
     if inplace:
         df[time_stamp_label] = df[time_stamp_label].values / sample_rate
         if time_label:
@@ -69,21 +108,26 @@ def timestamp2time(df, sample_rate, time_stamp_label, time_label=None, inplace=T
 
 
 
-def raster_by_unit(spikes, cluster, sample_rate, stim_length, window_size=1):
-    sns.set_context("notebook", font_scale=1.5, rc={'lines.markeredgewidth': .1, 'patch.linewidth':1})
+def raster_by_unit(spikes, cluster, sample_rate, window_size=1):
+    sns.set_context("notebook", font_scale=1.5, 
+                    rc={'lines.markeredgewidth': .1, 'patch.linewidth':1})
     sns.set_style("white")
     num_repeats = np.max(spikes['stim_presentation'].values)
     num_stims = len(np.unique(spikes["stim_name"]))
     g = sns.FacetGrid(spikes[spikes['cluster']==cluster], 
-        col="stim_name", col_wrap=int(np.sqrt(num_stims)), 
-        xlim=(-window_size, stim_length+window_size), ylim=(0,num_repeats));
-    g.map(plt.scatter, "stim_aligned_time", "stim_presentation", marker='|')
-    for ax in g.axes.flat:
-        ax.plot((0, 0), (0, num_repeats), c=".2", alpha=.5)
-        ax.plot((stim_length, stim_length), (0, num_repeats), c=".2", alpha=.5)
+        col="stim_name", col_wrap=int(np.sqrt(num_stims)));
+    g.map(_raster, "stim_aligned_time", "stim_presentation", "stim_duration", 
+          window_size=window_size)
     g = g.set_titles("cluster %d, stim: {col_name}" % (cluster))
-    plt.savefig('test.png')
 
+def _raster(stim_aligned_time, stim_presentation, stim_duration, window_size=1, **kwargs):
+    plt.scatter(stim_aligned_time, stim_presentation, marker='|', **kwargs)
+    num_repeats = np.max(stim_presentation)
+    stim_length = stim_duration.iloc[0]
+    plt.plot((0, 0), (0, num_repeats), c=".2", alpha=.5)
+    plt.plot((stim_length, stim_length), (0, num_repeats), c=".2", alpha=.5)
+    plt.xlim((-window_size, stim_length + window_size))
+    plt.ylim((0, num_repeats))
 
 from collections import Counter
 class _EventCounter(Counter):
@@ -92,7 +136,7 @@ class _EventCounter(Counter):
         return self[key] - 1
 
 def align_events(spikes, events, columns2copy=['stim_name', 'stim_presentation',
-                                               'stim_start', 'stim_end'],
+                                               'stim_start', 'stim_duration'],
                  start_label='stim_start', end_label='stim_end'):
     '''
     Generates a dataframe that labels spikes as belonging to event windows
@@ -106,7 +150,7 @@ def align_events(spikes, events, columns2copy=['stim_name', 'stim_presentation',
     spikes : Pandas.DataFrame
         from core.load_spikes
     events : Pandas.DataFrame
-        such as from load_acute_stims or core.get_trials
+        such as from load_acute_stims or core.load_trials
         must have non-overlapping windows defined by start_label and end_label
     columns2copy : iterable of strings
         labels of columns of events that you want to populate spike_stim_info_df
@@ -127,7 +171,9 @@ def align_events(spikes, events, columns2copy=['stim_name', 'stim_presentation',
     '''
     return pd.DataFrame(
         data=list(spikes["time_samples"].map(
-            _EventAligner(events, output_labels=columns2copy).event_checker)), 
+            _EventAligner(events, output_labels=columns2copy, 
+                          start_label=start_label, 
+                          end_label=end_label).event_checker)), 
         columns=columns2copy, index=spikes.index)
 
 class _EventAligner(object):
@@ -139,7 +185,8 @@ class _EventAligner(object):
         self.events = events
 
         event_columns = list(events.keys().get_values())
-        self.output_indices = [event_columns.index(lbl) for lbl in output_labels]
+        self.output_indices = \
+            [event_columns.index(lbl) for lbl in output_labels]
         self.start_index = event_columns.index(start_label)
         self.end_index = event_columns.index(end_label)
 
@@ -148,8 +195,10 @@ class _EventAligner(object):
         self.next_event = self.events.loc[self.event_index+1].values
     def event_checker(self, time_stamp):
         if time_stamp < self.cur_event[self.start_index]:
-            if self.event_index == 0 or self.cur_event[self.start_index] - time_stamp < time_stamp - self.prev_event[self.end_index]:
-                return self.cur_event[self.output_indices]
+            if self.event_index == 0 or \
+                self.cur_event[self.start_index] - time_stamp < \
+                time_stamp - self.prev_event[self.end_index]:
+                    return self.cur_event[self.output_indices]
             else:
                 return self.prev_event[self.output_indices]
         elif time_stamp < self.cur_event[self.end_index]:
--- ephys/rigid_pandas_test.py
+++ ephys/rigid_pandas_test.py
@@ -1,32 +1,71 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import rigid_pandas
-import core
+import rigid_pandas, core, events
 from timeit import default_timer as timer
+import os
 
-kwikfile = '/home/mthielk/Data/B979/acute/analysis/klusta/Pen04_Lft_AP2555_ML500__Site02_Z2688__st979_cat_P04_S02_1'
-kristas_kwikfile = '/mnt/cube/ephys-example-data/Pen01_Lft_AP100_ML1300__Site03_Z2500__B1056_cat_P01_S03_1'
+my_block = '/mnt/cube/ephys-example-data/long_acute_rec'
+kristas_block = '/mnt/cube/ephys-example-data/Pen01_Lft_AP100_ML1300__Site03_Z2500__B1056_cat_P01_S03_1'
+justins_block = '/mnt/cube/ephys-example-data/Pen01_Lft_AP2500_ML1350__Site10_Z2026__B997_cat_P01_S10_Epc10'
 
-block_path = kristas_kwikfile
+def test_acute(block_path):
+    start = timer()
+    try:
+        spikes, stims = rigid_pandas.kwik2rigid_pandas(block_path)
+    finally:
+        end = timer()
+        print "%s took %d seconds or %d minutes to load and align" % (os.path.split(block_path)[-1], end-start, (end-start)/60.0)
+    # print spikes.head()
+    # print stims.head()
 
-start = timer()
-spikes, stims = rigid_pandas.kwik2rigid_pandas(block_path)
-end = timer()
-print end-start
-print spikes.head()
-print stims.head()
-
-#spikes['morph_dim'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[0:2]
-#spikes['morph_pos'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[2:].astype(int)
+    #spikes['morph_dim'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[0:2]
+    #spikes['morph_pos'] = spikes[~spikes.stim_name.str.contains('rec')].stim_name.str[2:].astype(int)
 
 
-#rec_stims = good_spikes[good_spikes.stim_name.str.contains('rec')]
+    #rec_stims = good_spikes[good_spikes.stim_name.str.contains('rec')]
 
-clusters = core.load_clusters(block_path)
-fs = core.get_fs(block_path)
-stim_length = (stims.stim_end.head(1).values[0] - stims.stim_start.head(1).values[0]) / fs
-print clusters.head()
-cluster = clusters[clusters['quality']=="Good"]['cluster'].iloc[0]
-print stim_length
-rigid_pandas.raster_by_unit(spikes, cluster, fs, stim_length)
+    clusters = core.load_clusters(block_path)
+    fs = core.get_fs(block_path)
+    stim_length = (stims.stim_end.head(1).values[0] - stims.stim_start.head(1).values[0]) / fs
+    # print clusters.head()
+    cluster = clusters[clusters['quality']=="Good"]['cluster'].iloc[0]
+    rigid_pandas.raster_by_unit(spikes, cluster, fs)
+    plt.savefig('test.png')
+
+def test_chronic(block_path):
+    start = timer()
+    try:
+        spikes = core.load_spikes(block_path)
+        trials = events.get_trials(block_path)
+        trials['stim_start'] = trials['time_samples']
+        fs = core.get_fs(block_path)
+        trials['stim_duration'] = trials['stimulus_end'] - trials['time_samples']
+        rigid_pandas.timestamp2time(trials, fs, 'stim_duration')
+    finally:
+        end = timer()
+        print "%s took %d seconds or %d minutes to load" % (os.path.split(block_path)[-1], end-start, (end-start)/60.0)
+    start = timer()
+    try:
+        trials['stim_presentation'] = \
+            trials['stimulus'].map(rigid_pandas._EventCounter().count)
+        spikes = spikes.join(rigid_pandas.align_events(spikes, trials, columns2copy=['stimulus', 'stim_presentation',
+                                                                        'stim_start', 'stim_duration'],
+                                          start_label='time_samples', end_label='stimulus_end'))
+        spikes['stim_aligned_time'] = (spikes['time_samples'].values.astype('int') -
+                                       spikes['stim_start'].values)
+        del spikes['time_samples']
+        del spikes['stim_start']
+        rigid_pandas.timestamp2time(spikes, fs, 'stim_aligned_time')
+    finally:
+        end = timer()
+        print "%s took %d seconds or %d minutes to align" % (os.path.split(block_path)[-1], end-start, (end-start)/60.0)
+    clusters = core.load_clusters(block_path)
+    cluster = clusters[clusters['quality']=="Good"]['cluster'].iloc[0]
+    spikes['stim_name'] = spikes['stimulus']
+    rigid_pandas.raster_by_unit(spikes, cluster, fs)
+    plt.savefig('test2.png')
+
+test_acute(kristas_block)
+test_acute(my_block)
+test_chronic(justins_block)
